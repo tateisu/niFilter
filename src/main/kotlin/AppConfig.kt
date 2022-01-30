07fd1b5b6ee487ea7072a6adb1fb6c1309c4d1ef
tateisu <tateisu@gmail.com>
2022-01-30 19:50:31 +0900

    kts読み込み部分のコードを整理

--- src/main/kotlin/AppConfig.kt
+++ src/main/kotlin/AppConfig.kt
@@ -1,11 +1,12 @@
 import java.io.File
 import kotlin.script.experimental.annotations.KotlinScript
-import kotlin.script.experimental.api.*
+import kotlin.script.experimental.api.EvaluationResult
+import kotlin.script.experimental.api.ResultValue
+import kotlin.script.experimental.api.ResultWithDiagnostics
+import kotlin.script.experimental.api.ScriptDiagnostic
 import kotlin.script.experimental.host.toScriptSource
 import kotlin.script.experimental.jvm.dependenciesFromCurrentContext
 import kotlin.script.experimental.jvm.jvm
-import kotlin.script.experimental.jvm.util.isError
-import kotlin.script.experimental.jvm.util.isIncomplete
 import kotlin.script.experimental.jvmhost.BasicJvmScriptingHost
 import kotlin.script.experimental.jvmhost.createJvmCompilationConfigurationFromTemplate
 
@@ -19,48 +20,29 @@ abstract class KtsScript
 fun loadConfig(file: File) {
     val compilationConfiguration = createJvmCompilationConfigurationFromTemplate<KtsScript> {
         jvm {
-            // configure dependencies for compilation, they should contain at least the script base class and
-            // its dependencies
-
-            // variant 1: try to extract current classpath and take only a path to the specified "script.jar"
-            // script library jar name (exact or without a version)
-            // dependenciesFromCurrentContext("script" )
-
-            // variant 2: try to extract current classpath and use it for the compilation without filtering
             dependenciesFromCurrentContext(wholeClasspath = true)
-
-            // variant 3: try to extract a classpath from a particular classloader (or Thread.contextClassLoader by default)
-            // filtering as in the variat 1 is supported too
-            // dependenciesFromClassloader(classLoader = SimpleScript::class.java.classLoader, wholeClasspath = true)
-
-            // variant 4: explicit classpath
-            // updateClasspath(listOf(File("/path/to/jar")))
         }
     }
 
     val result = BasicJvmScriptingHost().eval(
-            file.toScriptSource(),
-            compilationConfiguration,
-            null
-        )
+        file.toScriptSource(),
+        compilationConfiguration,
+        null
+    )
 
     result.reports.forEach {
         if (it.severity > ScriptDiagnostic.Severity.DEBUG) {
             log.w(it.toString())
         }
     }
-    if( result !is ResultWithDiagnostics.Success){
-        error("script evaluation failed.")
-    }
 
-    when(val resultValue = result.value.returnValue){
-        is ResultValue.Value ->  when ( val newConfig =resultValue.value ) {
-            is AppConfig -> config = newConfig
-            null -> error("config script returns null.")
-            else -> error("config script return value is not AppConfig. ${newConfig.javaClass.simpleName}")
-        }
-        else-> error("resultValue is $resultValue")
-    }
+    result.castOrError<ResultWithDiagnostics.Success<EvaluationResult>> {
+        "script evaluation failed. $it"
+    }.value.returnValue.castOrError<ResultValue.Value> {
+        "script evaluation result is $it"
+    }.value.castOrError<AppConfig> {
+        "script returns value, but it's not AppConfig. ${it?.javaClass?.simpleName}"
+    }.let { config = it }
 }
 
 data class AppConfig(
@@ -95,4 +77,3 @@ data class AppConfig(
     // DB新規作成時にopmlファイルを読んでDBエントリを温める
     val subscriptions: String? = null,
 )
-
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -2,8 +2,14 @@ import kotlinx.coroutines.CoroutineScope
 import java.util.*
 import kotlin.coroutines.EmptyCoroutineContext
 
+inline fun <reified T:Any> Any.cast() :T? = this as? T
+
+inline fun <reified T:Any> Any?.castOrError(noinline messageCreator:(Any?)->String) =
+    (this as? T) ?: error(messageCreator(this))
+
 fun ByteArray.encodeBase64():String =
     Base64.getEncoder().encodeToString(this)
+
 
 object EmptyScope : CoroutineScope {
     override val coroutineContext = EmptyCoroutineContext
