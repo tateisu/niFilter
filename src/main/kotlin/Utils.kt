de5f11493d1390ed2973e683af64e5d285f34022
tateisu <tateisu@gmail.com>
2022-01-30 20:01:04 +0900

    コード整形

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -2,14 +2,13 @@ import kotlinx.coroutines.CoroutineScope
 import java.util.*
 import kotlin.coroutines.EmptyCoroutineContext
 
-inline fun <reified T:Any> Any.cast() :T? = this as? T
+inline fun <reified T : Any> Any.cast(): T? = this as? T
 
-inline fun <reified T:Any> Any?.castOrError(noinline messageCreator:(Any?)->String) =
+inline fun <reified T : Any> Any?.castOrError(messageCreator: (Any?) -> String) =
     (this as? T) ?: error(messageCreator(this))
 
-fun ByteArray.encodeBase64():String =
+fun ByteArray.encodeBase64(): String =
     Base64.getEncoder().encodeToString(this)
-
 
 object EmptyScope : CoroutineScope {
     override val coroutineContext = EmptyCoroutineContext
